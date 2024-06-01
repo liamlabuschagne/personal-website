1f5760431eb6f89003ec16ccad4212ac7eb4cafa
Liam Labuschagne <=>
2024-06-01 14:27:16 +1200

    Add first iteration of article about the making of this website.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <h1>Blog</h1>
-    </div>
+  <div>
+    <h1>Blog</h1>
+    <h2>Posts</h2>
+    <ul>
+      <li>
+        <nuxt-link to="/blog/this-website">This Website</nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>

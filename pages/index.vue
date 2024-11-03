23043cf5ed24aec913c5b1348dde1f508260a69b
Liam Labuschagne <=>
2024-11-04 08:29:07 +1300

    Add my name and description to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        <h1>Home</h1>
-        <NuxtLink to="/blog">Blog</NuxtLink>
-    </div>
+  <div>
+    <h1>Hi, I'm Liam Labuschagne</h1>
+    <p>
+      This is my personal slice of the internet where I put everything and
+      anything I want to share with the world. Check out the blog to see what
+      this website is made of!
+    </p>
+    <NuxtLink to="/blog">Blog</NuxtLink>
+  </div>
 </template>

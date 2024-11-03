04c52beff03cea5e403ddc1cfd52d7d3b97772d3
Liam Labuschagne <=>
2024-11-04 08:40:05 +1300

    Add links to home page

--- pages/index.vue
+++ pages/index.vue
@@ -7,5 +7,24 @@
       this website is made of!
     </p>
     <NuxtLink to="/blog">Blog</NuxtLink>
+    <h2>My Links</h2>
+    <p>The following are links to the rest of my digital presence.</p>
+    <ul>
+      <li>
+        <a href="https://github.com/liamlabuschagne" target="_blank">Github</a>
+      </li>
+      <li>
+        <a href="https://gitlab.com/liamlabuschagne" target="_blank">Gitlab</a>
+      </li>
+      <li>
+        <a href="https://nz.linkedin.com/in/liamlabuschagne" target="_blank"
+          >LinkedIn</a
+        >
+      </li>
+      <li>
+        <a href="https://thekiwiclick.co.nz/" target="_blank">Business</a>
+      </li>
+      <li><a href="https://linktr.ee/liamlab" target="_blank">Linktree</a></li>
+    </ul>
   </div>
 </template>

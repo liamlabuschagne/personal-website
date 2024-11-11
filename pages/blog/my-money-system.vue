6c6257aee80678294d5925a81b924a44b06c6a9f
Liam Labuschagne <=>
2024-11-12 10:24:06 +1300

    Introduction and step 1 and 2

---
+++ pages/blog/my-money-system.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <h1>My Money System</h1>
+    <p>
+      In this article I'll try to explain from some guiding principles how I
+      manage my money including bank accounts, investment accounts, KiwiSaver
+      and more. At each step I'll provide an overview of the options and justify
+      my choice of one of them. By each step I mean I'll build my system from
+      the ground up making adjustments as I go.
+    </p>
+  </header>
+  <section>
+    <h2>Step 1: Income</h2>
+    <p>
+      This might seem too basic but I think it more accurately aligns with my
+      process. I'm starting with income because for most of us, if not all of
+      us, we need money to survive. Even in retirement, where ever it may come
+      from whether it be from the government, a pension or investments, an
+      income is a helpful way of managing our money because it creates a useful
+      rhythm or pace for consumption.
+    </p>
+    <p>
+      The alternative here would be to have a huge pot of money (whether in cash
+      or bank deposits) and slowly spend this down over the course of our lives.
+      An obvious problem with this is, firstly that most people don't have a
+      huge pot of money just lying around, at least not when they first start
+      out in life and secondly, what happens if you run out of money before you
+      die? So for this reason I'm assuming your system will also involve a
+      source or sources of income.
+    </p>
+  </section>
+  <section>
+    <h2>Step 2: A Bank Account</h2>
+    <p>
+      Why do we have bank accounts? One reason might be that our employer would
+      rather deposit money directly into a bank account rather than hand over a
+      wad of cash every month for everyone to see, keeping in mind that this is
+      the reality for many people around the world, how cool would that be?
+    </p>
+  </section>
+</template>
+<style scoped>
+header,
+section {
+  max-width: 900px;
+  margin: auto;
+}
+
+img {
+  max-width: 100%;
+}
+</style>

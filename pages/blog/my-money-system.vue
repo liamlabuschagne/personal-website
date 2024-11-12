19e392af5b7899805c99d6fb55e2bafcf2f7b473
Liam Labuschagne <=>
2024-11-12 21:17:12 +1300

    More progress on money system article

--- pages/blog/my-money-system.vue
+++ pages/blog/my-money-system.vue
@@ -10,7 +10,7 @@
     </p>
   </header>
   <section>
-    <h2>Step 1: Income</h2>
+    <h2>Part 1: Income</h2>
     <p>
       This might seem too basic but I think it more accurately aligns with my
       process. I'm starting with income because for most of us, if not all of
@@ -30,12 +30,215 @@
     </p>
   </section>
   <section>
-    <h2>Step 2: A Bank Account</h2>
+    <h2>Part 2: A Bank Account</h2>
     <p>
       Why do we have bank accounts? One reason might be that our employer would
       rather deposit money directly into a bank account rather than hand over a
       wad of cash every month for everyone to see, keeping in mind that this is
       the reality for many people around the world, how cool would that be?
+      Walking away after each day's work with the money in hand.
+    </p>
+    <h3>Pay Frequency</h3>
+    <p>
+      That brings up the question around frequency of pay. Around the world, how
+      frequently people get paid changes based on employment laws. Normally,
+      companies prefer to pay as infrequently as possible because it reduces
+      overheads involved with administration, especially having 12 fixed dates
+      in a year as opposed to bi-weekly pay for example which gets complicated
+      when you have leap years or months with 3 pay periods. For employees,
+      being paid more frequently is preferred because it helps to manage cash
+      flow. You can read more about this on
+      <a
+        target="_blank"
+        href="https://www.cloudpay.com/blog/pay-frequency-around-the-world/"
+      >
+        an article by CloudPay</a
+      >.
+    </p>
+    <h3>Business Mindset</h3>
+    <p>
+      Companies however usually use an invoicing model whereby goods or services
+      delivered in one month are due to paid for the following month on a set
+      day like the 20th. As a partly self-employed person, I've adopted this
+      mindset because it makes you less reliant on the income coming in and
+      forces you to be more disciplined and have longer term thinking going from
+      a weekly mindset to a monthly or quarterly way of thinking. Additionally,
+      it means you don't stress about overspending in a short time period like a
+      week because you know that over a 3 month period everything should even
+      out with some months being less busy others following the natural ebbs and
+      flows of life.
+    </p>
+  </section>
+  <section>
+    <h2>Part 3: Expenses</h2>
+    <p>
+      It would be nice to have a world where every month you earn income and it
+      just piles up over time but we do have living costs to cover. You can
+      think of this as the cost of doing business, everyone has to sleep
+      somewhere, eat something and wear something and of course we pay taxes
+      along the way on everything as well.
+    </p>
+    <h3>Forms of Payment</h3>
+    <p>
+      Most people think of paying for something as a real time transaction where
+      you pull out some cash, a card or initiate a bank transfer but in our
+      modern world we pay for things in so many other ways as well. For example,
+      your mortgage might be gobbled up by the bank as soon as you get paid and
+      you don't even have to do anything or remember to pay it, that's called a
+      direct debit, the company has the ability to directly pull money out of
+      your bank account.
+    </p>
+    <p>
+      You might also have a regular payment setup from your account which
+      automatically pays your rent or another bill. Finally, some companies
+      prefer you to pay with a debit or credit card since they can charge the
+      card whenever they want and don't have to chase you up about late payment,
+      instead just cutting off services.
+    </p>
+    <h3>Missing a Payment</h3>
+    <p>
+      Because of the sheer number of payments and lack of innovation in banking
+      software it can be difficult to keep track of all these payments coming
+      out on different days and frequencies, sometimes even changing in amount
+      based on usage. If you overspend and don't have enough money in your
+      account you may go into overdraft where you create an unarranged temporary
+      loan from the bank usually with very high interest and penalty fees. This
+      is completely avoidable with a little bit of organisation.
+    </p>
+    <p>
+      A similar issue is with credit cards, because they usually have something
+      like 55 interest free days on purchases, you might end up in a situation
+      where you overspend and when the credit card is due to be paid off you
+      don't have enough funds so either your bank account goes into overdraft or
+      you are left with a balance accruing interest. Again, this is completely
+      avoidable.
+    </p>
+  </section>
+  <section>
+    <h2>Part 4: A Second Bank Account</h2>
+    <p>
+      How is it avoidable? Well, one approach is to always have a buffer in your
+      bank account so regardless of when or how much is charged to it,
+      everything gets paid on time without you having to do anything. This
+      certainly reduces stress but you do run the risk of seeing a much higher
+      balance and subconsciously thinking you have more money to spend than you
+      actually have so you can still end up going through this buffer and
+      spending money on movie tickets that should have gone towards the water
+      bill the next day.
+    </p>
+    <p>
+      To avoid this, I propose having a second bank account into which you pay
+      yourself an allowance for day to day spending. Crucially, only manual
+      purchases should come from this account, with all automatically deducted
+      expenses being covered by your first account. This way you can always see
+      exactly how much you have left to spend and you will never need to worry
+      that you are spending money needed for a bill because that is handled by a
+      completely separate account.
+    </p>
+    <p>
+      Even with this system, you may still get your budget wrong or one month
+      have a high bill for electricity for example so it is still advisable to
+      keep a buffer here to keep everything running smoothly, a margin of error
+      if you will.
+    </p>
+  </section>
+  <section>
+    <h2>Part 5: Losing Your Income</h2>
+    <p>
+      So far, this whole system has assumed that you will have an income coming
+      in and that it is the same amount each month. For many people this is not
+      the case, especially if you work an hourly job where your pay can
+      fluctuate widely throughout the year or worse, you don't have any income
+      at all for an extended period of time.
+    </p>
+    <p>
+      This is where the concept of savings comes in. We have already been saving
+      in our system by another name: buffers. So one approach to this would be
+      to just increase this buffer to an amount where you can go for multiple
+      months at a time or at least weather the ups and downs of a variable
+      income.
+    </p>
+    <p>
+      This however introduces the same problem as we had before whereby because
+      we have a larger pile of money we are inclined to spend it down. It is
+      possible that you spend all your buffer assuming your income will top it
+      up but the income never comes.
+    </p>
+    <p>
+      To avoid this risk, I propose having a third bank account with a few
+      months worth of savings. In other words, an amount that you could live off
+      of assuming you had no other income for that period. The account should be
+      separate because it helps remind you to not dip into that money in normal
+      circumstances, only when you have a dramatic reduction in income.
+    </p>
+    <p>
+      Another benefit of having this amount set aside is that it acts as a
+      cushion between you and debt. If you treat it right, you will have the
+      peace of mind knowing that if all income stops you have a few months to
+      find new employment or otherwise come up with a new plan without paying
+      avoidable fees and interest.
+    </p>
+  </section>
+  <section>
+    <h2>Part 6: Save now, Pay Later</h2>
+    <p>
+      Sometimes you have expenses that are too large to cover with your normal
+      spending allowance. For example, you might need to buy a car, want to go
+      on a holiday or pay insurance once per year to get a discount. It would be
+      annoying if every time one of these expenses comes up you then have to
+      rein back your spending for that month just to stay in the budget,
+      sometimes this may not be possible because it is an amount larger than a
+      month's worth of spending.
+    </p>
+    <p>
+      You could pay for this from your savings account from before but this
+      would put you at risk if your income changes and the buffer in your first
+      bank account is designed to protect you from overdrafts so where does this
+      money come from?
+    </p>
+    <p>
+      For many people the solution here is to put this purchase on debt of some
+      kind, again incurring avoidable interest and fees. In recent years, buy
+      now pay later schemes have attempted to solve this problem for consumers
+      by spreading out this large sum over multiple payments.
+    </p>
+    <p>
+      If you know that this expense is coming, especially something like a car
+      registration or yearly insurance, why not plan ahead, putting money aside
+      each month until the expenses comes up. To prevent confusion I propose
+      having a fourth bank account for this purpose. You may even want to
+      contribute more to this account than you expect your future expenses will
+      be as a buffer in case that expense turns out to be higher than you
+      thought.
+    </p>
+    <p>
+      So, when that large expense comes up, you simply top up the account from
+      which you pay for it and continue your spending as normal. This
+      essentially smooths out a irregular expense to be a regular one. Instead
+      of buy now, pay later you are saving now to pay later.
+    </p>
+  </section>
+  <section>
+    <h2>Part 7: Optimising</h2>
+    <p>
+      If you follow the advice given above including understanding your income,
+      how frequently you get paid, what bank accounts to setup and leveraging
+      buffers and savings to handle the fluctuations of life you, will be doing
+      well. In fact, you may find yourself not really thinking about money on a
+      daily or even weekly basis any more. Only checking up on your spending and
+      making transfers for big purchases as they come along, everything else is
+      automatic and stress free.
+    </p>
+    <p>
+      But what if you want to do one better? All throughout this article we have
+      developed a money system that avoids volatility, reduces banking admin and
+      avoids unnecessary fees and interest. What if you could turn the tables
+      and earn interest for how well you organise your finances?
+    </p>
+    <p>
+      I propose you should have as many of your accounts be interest bearing as
+      possible as long as they don't impede on your payments being made like
+      withdrawal limits or minimum balance requirements.
     </p>
   </section>
 </template>

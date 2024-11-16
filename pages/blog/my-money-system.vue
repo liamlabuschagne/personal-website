ba20c2fbc082a8ca954a5291254921449761912d
Liam Labuschagne <=>
2024-11-17 00:34:18 +1300

    Pretty much complete

--- pages/blog/my-money-system.vue
+++ pages/blog/my-money-system.vue
@@ -2,104 +2,38 @@
   <header>
     <h1>My Money System</h1>
     <p>
-      This article will explain exactly how I manage my money in every aspect
-      from earning, spending and saving. Particularly, I'll explain how I've
-      setup my financial accounts and how I use them in every situation.
+      This article explains a system that I've developed for managing money and
+      ultimately helping achieve what I want out of life. Money isn't everything
+      and it certainly should not be our main focus but it is a useful tool and
+      it should be handled well.
+    </p>
+    <p>
+      Generally, I like to run my personal finances like a business where
+      (financially) the goal is to increase my savings rate (profit) and my net
+      worth (equity). Your savings rate is the difference between your income
+      and expenses whilst your net worth is the difference between what you own
+      (assets) and what you owe (liabilities).
+    </p>
+    <p>
+      The motivation for these is firstly to be a good steward of the resources
+      given to me by God and secondly to be prepared financially for everything
+      life has to offer whether good or bad. This is elsewhere known as
+      financial independence which is something you want to achieve before you
+      retire at least and earlier if possible.
     </p>
   </header>
-  <section>
-    <h2>Requirements</h2>
-    <p>
-      Before I start, I'd like to set out some requirements for this money
-      system (as I am an engineer after all), which will be used to justify the
-      decisions I make about its design as I develop it throughout this article.
-    </p>
-    <ul>
-      <li>
-        <strong>Simplicity</strong>: It should be easy to use and easy to
-        understand in my head without any complex diagrams or explanations.
-      </li>
-      <li>
-        <strong>Efficiency</strong>: I want to spend as little time and energy
-        managing my money as possible.
-      </li>
-      <li>
-        <strong>Versatile</strong>: The system should be flexible and robust
-        enough to handle all life stages and unpredictable events without major
-        adaptations.
-      </li>
-    </ul>
-  </section>
-  <section>
-    <h2>Earning, Spending and Saving</h2>
-    <p>
-      Next, I want to layout my philosophy of money and all the ways it can be
-      used. Obviously, to get money you need to earn it in some form usually
-      referred to as income. You then have expenses necessary to keep you alive
-      and to get enjoyment out of life.
-    </p>
-    <p>
-      What's left over is not savings, savings is an explicit choice to set
-      aside money for your future self. Money not spent but also not explicitly
-      saved will be spent eventually. This is also why getting a discount on an
-      item is not saving money because you will likely still spend that money
-      somewhere else.
-    </p>
-  </section>
-  <section>
-    <h2>Assets, Liabilities and Equity</h2>
-    <p>
-      Just as a business has things it owns and amounts it owes, so do we. Whats
-      left is called equity, also known as net worth in personal finance terms
-      (which is an unfortunate framing but people understand this).
-    </p>
-    <p>
-      Money explicitly saved and not spent is the only way to increase your net
-      worth in a way you fully control. This is unlike paying down debt where
-      your liabilities are reduced but so are your assets, the difference
-      between the two has not changed.
-    </p>
-    <p>
-      The only other way your net worth (or equity) changes is if the value of
-      your assets increase, but this is not something you can really control, so
-      it should not be focused on. It is more important to focus on accumulating
-      assets with the money you explicitly save.
-    </p>
-  </section>
-  <section>
-    <h2>Why strive for net worth?</h2>
-    <p>
-      Financially speaking, net worth is the only number that matters in the
-      long term. If you make a trillion dollars a year but also spend a trillion
-      dollars that's money you'll never get back and at the end of that year you
-      will have nothing to show for it.
-    </p>
-    <p>
-      This is important because income is not guaranteed and some day it will
-      come to an end (either by choice or by force) yet you still need it to
-      live. Therefore having some money set aside to live on in the future is
-      vital and it gives you more choices in life.
-    </p>
-  </section>
-  <section>
-    <h2>Bank Accounts</h2>
-    <p>
-      Below I've named the three accounts you will need for this system and what
-      type of account they are.
-    </p>
-    <ol>
-      <li>Central account (Transactional)</li>
-      <li>Spending account (Transactional)</li>
-      <li>Savings account (Savings)</li>
-    </ol>
-  </section>
   <section>
     <h2>Earning</h2>
     <p>
       For this system to work, you will need to decide on a single monthly
       income number even if your income is variable. The system will accommodate
       for fluctuations but this number will be the basis of spending and saving
-      calculations so choose a conservative amount (e.g low average).
+      calculations so choose a conservative amount (e.g a low average).
+    </p>
+    <p>
+      It is important that all your income is deposited into your central
+      account which is just any bank account that gives you all the features you
+      need with minimal fees.
     </p>
   </section>
   <section>
@@ -136,15 +70,18 @@
       through the month and have less than half your allowance left, try to rein
       in your spending.
     </p>
-  </section>
-  <section>
-    <h2>Buffers</h2>
     <p>
-      A one month buffer in each transactional account will handle any
-      deviations from the planned amounts and mean you are always paying this
-      months expenses with last months income. Less expensive months will
-      overflow the buffer and more expensive months will consume this overflow
-      to create a fairly consistent average.
+      <strong>Buffers:</strong> A one month buffer in each transactional account
+      will handle any deviations from the planned amounts and mean you are
+      always paying this months expenses with last months income. Less expensive
+      months will increase the buffer and more expensive months will consume
+      this increase to create a fairly consistent average.
+    </p>
+    <p>
+      <strong>Maintenance:</strong> At the start of every month, top up the
+      buffers from your savings account if needed. If your buffers are
+      increasing consistently, just increase your savings contribution or your
+      spending allowance, don't do more transfers than are necessary.
     </p>
   </section>
   <section>
@@ -156,24 +93,25 @@
       your spending allowance so choose a realistic amount and commit to it.
     </p>
     <p>
-      Some expenses are not able to be easily absorbed by a spending allowance
-      so they can be saved for ahead of time by noting the amount and when they
-      are due in a list. These can then be drawn from savings before paying for
-      them from the spending account or at the monthly maintenance session if
-      needed in the central account.
+      <strong>Future expenses list:</strong> Some expenses are not able to be
+      easily absorbed by your spending allowance so they can be saved for ahead
+      of time by noting the amount and when they are due in a list. These can
+      then be drawn from savings just before paying for them from the spending
+      account or at the monthly maintenance session if the expense is automatic
+      and will come out of the central account.
     </p>
     <p>
-      For unplanned, necessary expenses or in the case that you lose your income
-      you will also need some savings set aside, note this on the list as a
-      single sum, it will act as a floor for the savings account. This can be
+      For unplanned but necessary expenses or in case you lose your income, you
+      will also need some savings set aside, note this on the list as a single
+      sum, it will act as a floor for your savings account. This can be
       estimated based on historical trends or otherwise 3-12 months of living
       expenses based on how stable and secure your income is.
     </p>
     <p>
-      Based on the list produced, a total required amount should be calculated.
-      Subtracting this from the current balance will reveal either a surplus or
-      deficit for these future expenses when forecasting future savings
-      contributions.
+      <strong>Saving for the list:</strong> Based on the list produced, a total
+      required amount should be calculated. Subtracting this from the current
+      balance will reveal either a surplus or deficit for these future expenses
+      when forecasting future savings contributions.
     </p>
     <p>
       If a surplus, this can be allocated to future spending by adding more
@@ -186,14 +124,19 @@
       otherwise less important expenses should be removed or scaled down or the
       savings rate increased, reducing the spending allowance.
     </p>
-  </section>
-  <section>
-    <h2>Maintenance</h2>
     <p>
-      At the start of every month, top up the buffers from your savings account
-      if needed. If your buffers are increasing consistently, just increase your
-      savings contribution or your spending allowance, don't do more transfers
-      than are necessary.
+      <strong>Investing:</strong> If there is a surplus and all known future
+      expenses have been accounted for, the savings contribution should be
+      redirected to a higher performing investment like a shares portfolio which
+      can be contributed to automatically every month.
+    </p>
+    <p>
+      Investing should should be done along side regular savings since it will
+      produce a better return than cash and can one day be used to subsidise or
+      fully fund your lifestyle. This is done by amassing a large enough
+      portfolio such that the yearly returns are larger than your required
+      withdrawals. A good target is 25 times your annual expenses which
+      represents a 4% withdrawal rate.
     </p>
   </section>
 </template>

268ac3d4d23ba82022a869a6c1573ea295ac57e6
Liam Labuschagne <=>
2024-11-23 15:13:35 +1300

    Savings accounts article

---
+++ pages/blog/savings-accounts.vue
@@ -0,0 +1,209 @@
+<template>
+  <header>
+    <h1>Savings Accounts</h1>
+    <p>
+      In this article I'll explain everything you need to know about savings
+      accounts including what they are, the types, when to use them and how to
+      choose one. In
+      <nuxt-link to="/blog/my-money-system">my money system</nuxt-link> article
+      I explain how your savings account fits into your overall financial system
+      so check that out if you haven't already.
+    </p>
+  </header>
+  <section>
+    <h2>What is a savings account?</h2>
+    <p>
+      Many people get confused about this surprisingly, conflating many types of
+      financial accounts and investments as savings accounts when they aren't
+      the same thing. Savings accounts are accounts offered by
+      <a
+        href="https://www.rbnz.govt.nz/regulation-and-supervision/cross-sector-oversight/registers-of-entities-we-regulate/registered-banks-in-new-zealand"
+        target="_blank"
+      >
+        registered banks</a
+      >
+      or
+      <a
+        href="https://www.rbnz.govt.nz/regulation-and-supervision/cross-sector-oversight/registers-of-entities-we-regulate/register-of-non-bank-deposit-takers-in-new-zealand"
+        target="_blank"
+        >non-bank deposit takers</a
+      >
+      that pay interest on your deposit.
+    </p>
+    <p>
+      The money you put into these accounts are not invested into any other type
+      of asset, they are simply held by the bank until such time that you want
+      to withdraw it.
+    </p>
+    <p>
+      In the mean time banks can use the money to allow them to create more
+      loans but this is quite a complex topic so I won't get any further into
+      those mechanics here.
+    </p>
+    <p>
+      This means that fundamentally, the only risk your money is exposed to is
+      the risk of that bank failing and otherwise the value of your deposit will
+      not fluctuate like other assets (other than earning interest on top of
+      your original deposit).
+    </p>
+  </section>
+  <section>
+    <h2>Types</h2>
+    <p>
+      The only difference between savings accounts are the interest rate you get
+      and how restricted you are to withdrawing the funds. Generally, the more
+      restrictive the account is, the more interest it will pay you for that
+      commitment. This is because banks can then more easily plan on having a
+      certain level of deposits.
+    </p>
+    <p>These are the three types of savings accounts:</p>
+    <ul>
+      <li>On-call</li>
+      <li>Notice</li>
+      <li>Term Deposit</li>
+    </ul>
+    <p>
+      On-call means that you can withdraw the money at any time without notice.
+      Some on-call savings accounts will have a bonus interest rate if you meet
+      certain requirements like increasing the balance by $50 per month or not
+      making any withdrawals. Sometimes banks will even charge you fees if you
+      make too many withdrawals.
+    </p>
+    <p>
+      Notice savings accounts are exactly the same but require to give a certain
+      amount of notice like 30 or 90 days which practically means you initiate a
+      withdrawal and choose a date for that withdrawal at least that far into
+      the future. In the mean time you can continue to deposit money into it as
+      normal and you will earn whatever the current interest rate is.
+    </p>
+    <p>
+      A term deposit is the most restrictive where you actually lock in both the
+      term and the interest rate. Because of this they generally have the
+      highest interest rates available.
+      <a
+        href="https://bankomb.org.nz/guides-and-cases/quick-guides/savings-and-investment/breaking-a-term-deposit"
+        >You have no protections under the law</a
+      >
+      to get access to this money early unless you come to an agreement with
+      your bank, some banks have looser restrictions and regulations than
+      others.
+    </p>
+    <p>
+      Each of these types of accounts can be offered as a PIE (Portfolio
+      Investment Entity) which is a tax structure originally created for
+      KiwiSaver that essentially caps your tax rate at 28% (which is the company
+      tax rate) so if you are in a higher personal income tax bracket you should
+      seriously consider accounts with this tax structure.
+    </p>
+  </section>
+  <section>
+    <h2>How much should I keep in savings?</h2>
+
+    <p>
+      Firstly, I'll say that as a general rule because the risk of savings
+      accounts are very low (the value of your deposit doesn't change) you will
+      on average be getting a lower return on your money than some kind of
+      investment (who's value does fluctuate) because you are taking on more
+      risk. This means you should actually try to minimise your how much you
+      keep in "cash" (which are physical cash and bank deposits) because you are
+      paying an opportunity cost as well as inflation eating away at the value
+      of your money.
+    </p>
+    <p>
+      Infact, interest rates are generally lower than the rate of inflation
+      because central banks use this to stimulate or cool down the economy to
+      keep inflation within an acceptable range. So you could argue that money
+      kept in a savings account will on average always be loosing money to
+      inflation and therefore should not be thought of as an investment.
+    </p>
+    <p>
+      Therefore, your cash position should only be just enough to cover your day
+      to day spending between paydays and some extra for unplanned expenses that
+      may come up. Beyond this, you should only have more in savings if you are
+      looking to use that money within a few years for say a house deposit. The
+      rest should be invested to get a higher return.
+    </p>
+    <p>
+      The only exception to this rule may be if you are entering retirement and
+      are more interested in creating an income portfolio but again that is not
+      the focus of this article and if you are unsure of anything you should
+      seek financial advice for all matters especially retirement planning.
+    </p>
+  </section>
+  <section>
+    <h2>How to choose a savings account?</h2>
+    <p>
+      If then we accept that savings accounts are not investments and are simply
+      less bad than keeping money in a no-interest account or in cash, then it
+      makes sense that we want to save our money in accounts with minimal
+      restrictions and no fees (there is no reason to be paying fees on a
+      savings account).
+    </p>
+    <p>
+      This means that you should be looking for the highest interest rate you
+      can get with the least restrictions. You can use comparison websites like
+      <a href="https://www.interest.co.nz/saving/e-saver-online" target="_blank"
+        >interest.co.nz</a
+      >
+      to compare the interest rates of different accounts.
+    </p>
+    <p>
+      I've even made a
+      <a
+        target="_blank"
+        href="https://addons.mozilla.org/en-US/firefox/addon/rate-ranker/"
+        >firefox</a
+      >
+      or
+      <a
+        href="https://chromewebstore.google.com/detail/rate-ranker/cjonainnbhogknlllalnmmlgkjoombnm"
+        target="_blank"
+        >chrome</a
+      >
+      extension that allows you to automatically sort the tables on this website
+      by interest rate. Remember to check the
+      <a
+        href="https://www.interest.co.nz/credit-ratings-explained"
+        target="_blank"
+        >credit rating</a
+      >
+      of the institution and make sure you are comfortable keeping your money
+      with them.
+    </p>
+    <iframe
+      width="500"
+      height="282"
+      src="https://www.youtube.com/embed/Ch69I7GnSUQ"
+      title="Rate Ranker Extension for interest.co.nz"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  </section>
+</template>
+<style scoped>
+* {
+  font-family: sans-serif;
+}
+
+p,
+li {
+  font-size: large;
+}
+
+header,
+section {
+  max-width: 900px;
+  margin: auto;
+}
+
+img {
+  max-width: 100%;
+}
+
+iframe {
+  display: block;
+  margin: auto;
+}
+</style>

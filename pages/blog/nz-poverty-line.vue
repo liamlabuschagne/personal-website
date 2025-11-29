6f2b320f45631bf9b1e3745893d99141ef5ac1b8
Liam Labuschagne <liaml@disroot.org>
2025-11-29 22:26:44 +1300

    Add NZ Poverty line blog

---
+++ pages/blog/nz-poverty-line.vue
@@ -0,0 +1,229 @@
+<template>
+    <h1>New Zealand Poverty Line</h1>
+    <p>
+        See the <a
+            href="https://docs.google.com/document/d/14p9IxbvnxUtePhE6W_XVnHcENXuoTKrTSNB8mRrbZUo/edit?usp=sharing">original
+            Google Doc</a>
+    </p>
+    <p>
+        Inspired by <a href="https://www.yesigiveafig.com/p/part-1-my-life-is-a-lie">a US article claiming a poverty
+            line of
+            US$140k</a> (NZ$240) gross income for a family with two kids (this is the poverty line not comfortable).
+    </p>
+    <p>
+        I'm interested in what this is in NZ assuming two income earners, two kids living in Onehunga.
+    </p>
+    <p>
+        The IRD has their own <a
+            href="https://www.ird.govt.nz/-/media/project/ir/home/documents/forms-and-guides/ir100---ir199/ad164/ad164-2025-may.pdf?modified=20250515232207&modified=20250515232207">Household
+            Expenditure Guide</a> which you can compare to if you like.
+    </p>
+    <h2>Expenses</h2>
+    <h3>Housing</h3>
+    <p>
+        <a
+            href="https://www.tenancy.govt.nz/rent-bond-and-bills/market-rent/?location=Auckland+-+Onehunga&period=31&action_doSearchValues=Find+Rent">Tenancy
+            Services rental data for Auckland - Onehunga</a>
+    </p>
+    <p>
+        I'm going to use rent as a convenient proxy for the total cost of home ownership (including maintenance,
+        renovations
+        and mortgage interest) especially since at the moment it is cheaper to rent.
+        A 3 bedroom house in the lower quartile of rent in Auckland - Onehunga is $694/week.
+    </p>
+    <h3>Food</h3>
+    <p>
+        Food: $100-150 per person so $400-600 per week for a family of four.
+    </p>
+    <p>
+        Based on <a
+            href="https://www.naumainz.studywithnewzealand.govt.nz/studying-in-nz/before-your-arrival/cost-of-living">"Study
+            with New Zealand" recommendations for potential students.</a>
+    </p>
+    <p>
+        Another source could be the <a
+            href="https://ourarchive.otago.ac.nz/esploro/outputs/report/Information-for-users-of-the-New/9926478644401891">Food
+            Cost Survey from 2023</a> by University of Otago except this is already two years old. They also don't
+        include
+        any takeways at all. They estimate an Auckalnd family with two kids to be "Liberal" at $412 a week which seems
+        quite
+        low. I will take $500/week.
+    </p>
+    <h3>Transportation</h3>
+    <p>
+        Initially I calculated this based on <a href="https://www.ird.govt.nz/income-tax/income-tax-for-businesses-and-organisations/types-of-business-expenses/claiming-vehicle-expenses/kilometre-rates-2024-2025
+https://www.ird.govt.nz/vehicle-expenses">IRD vehicle kilometer rates</a> for one car used the average kms per year so
+        $315/week ($1.17 x 14,000km/year).
+    </p>
+    <p>
+        Comparing this to other sources like the IRD Household Expenditure and <a
+            href="https://www.stats.govt.nz/news/transport-expenditure-both-rises-and-falls-for-new-zealand-households/">2023
+            Census data</a> I will adjust this downward to a more basic $250 per week.
+    </p>
+    <h3>Childcare</h3>
+    <p>
+        This is for holiday programmes during school holidays (both are 5+).</p>
+    <p>
+        <a href="https://www.education.govt.nz/school/school-terms-and-holiday-dates">NZ School Term Dates 2025</a>
+    </p>
+    <p>
+        <a href="https://www.govt.nz/browse/work/public-holidays-and-work/public-holidays-and-anniversary-dates/">NZ
+            Public
+            Holidays</a>
+    </p>
+    <p>
+        <a href="https://www.employment.govt.nz/leave-and-holidays/annual-holidays">NZ Annual Holidays</a> - 4 weeks
+        annual
+        leave (20 days).
+    </p>
+    <p>
+        I'm only counting Monday-Friday since weekends you get off work anyway (typically), so therefore 5 days per
+        school
+        holiday week less any public holidays.
+        Term 1 - 10 days less 3 public holidays so 7 days<br>
+        Term 2 - 10 days<br>
+        Term 3 - 10 days<br>
+        Summer holidays = 6 weeks x 5 days = 30 days less 4 public holidays so 26 days <br>
+        New Year's Day, Day after New Year's Day, Christmas Day, Boxing Day <br>
+        <br>
+        So total of 7 + 10 + 10 + 26 = 53 days annual leave <br>
+        Each has 20 days AL so 40 total. 53 - 40 = 13 days childcare
+    </p>
+    <p>
+        <a href="https://www.moneyhub.co.nz/auckland-school-holiday-activities.html">Auckland School Holiday
+            Activities</a>
+    </p>
+    <p>
+        Youthtown - $45 per child per full day - $90 per day for two kids <br>
+        $90/day x 13 days required = $1,170 per year
+    </p>
+    <h3>Power</h3>
+    <p>
+        $3,063.06 per year or $58.91/week. <br>
+        Based on data from MBIE <a href="https://www.mbie.govt.nz/assets/qsdep-report-15-August-2025.pdf">"Quarterly
+            Survey
+            of Domestic Electricity Prices"</a>
+        <br>
+        Based on 22kW per day or 8,000kW per year at 38.25c/kW or $3,063.06 per year
+    </p>
+    <h3>Rates</h3>
+    <p>
+        $78.25/week <br>
+        Based on <a
+            href="https://ourauckland.aucklandcouncil.govt.nz/news/2025/08/council-rates-here-s-how-it-works/">Auckland
+            Council latest average rates data.</a> <br>
+        $4,069 per year for average property
+    </p>
+    <h3>Water</h3>
+    <p>
+        $25/week <br>
+        <a
+            href="https://www.rnz.co.nz/news/national/547500/auckland-water-charges-set-to-increase-7-point-2-percent-from-july">RNZ
+            article talking about most recent Auckland water rates rise</a> <br>
+        “From 1 July 2025, the price an average Auckland household pays for water and wastewater services would go up
+        7.2
+        percent or $7 a month.” so average must be $100/month
+    </p>
+    <h3>Internet</h3>
+    <p>
+        Based on a <a href="https://www.moneyhub.co.nz/compare-broadband.html">comparison on MoneyHub</a> 300 mbps is
+        $90
+        per month. You can get this down to about $60 per month for the most basic plan but this might not be enough for
+        a
+        family of four.
+    </p>
+    <h3>Phone Plan</h3>
+    <p>
+        Again, based on a <a href="https://www.moneyhub.co.nz/compare-mobile-plans.html">MoneyHhub comparison of mobile
+            plans</a> Warehouse mobile $15 combo, just the parents need one each so $30 per 28 days = 13 months or $390
+        annually = $7.50 per week
+    </p>
+    <h3>Home Contents Insurance</h3>
+    <p>
+        Based on a <a href="https://www.moneyhub.co.nz/contents-insurance.html">MoneyHub comparison of home contents
+            insurance</a> this is $400 to $600 per year for $25,000 of total cover. I'll take the middle number of $500.
+    </p>
+    <h3>Life Insurance</h3>
+    <p>
+        Based on a <a href="https://www.moneyhub.co.nz/life-insurance.html">MoneyHub comparison of life insurances</a>
+        <br>
+        “30-year-olds: Around $400 annually for $500,000 cover”
+        Sounds like it's usually just on one person's income (higher earner).
+    </p>
+    <h3>Other</h3>
+    <p>
+        Because more often than not when creating hypothetical budgets you underestimate your actual costs I'm going to
+        add
+        a blanket $100 a week as a margin of error.
+    </p>
+    <h2>Income</h2>
+    <p>
+        See the Google sheets for full calculation of <a
+            href="https://docs.google.com/spreadsheets/d/1fX5Tm09ElxVxbY2u85o1ZKiE2_jmd8wG_rJNnY5XJHM/edit?usp=sharing">my
+            New Zealand Poverty Line</a>. <br> <br>
+        My sheet estimates ~$92k per year of household expenses. If I round this up to $100k, under the most tax
+        efficient situation where both earners are earning the same to achieve this after tax income they both need $50k
+        after tax. I will assume they at least take the minimum 3% employer KiwiSaver match.
+        <br>
+        That is $63,830.56 before tax per earner or $127,661.12 total household income. <br>
+        See <a href="https://regions.infometrics.co.nz/auckland/income-and-housing/household-income">InfoMetrics median
+            household income data</a> <br>
+        Compared to median household income in Auckland of $166k or $39k before tax more than the poverty line. Again,
+        in the most tax efficient situation the household income is halved so $83,000 each or $61,856.40 after tax and
+        KS. This is $11k per earner above the poverty line or $22k per household of savings/holiday money. <br>
+        <br>
+        For comparison the median household income in NZ is only $135k or $8k above the poverty line.
+        <br>
+        If we assume there is only one income earner the numbers work out like this: <br>
+        To achieve the $100k poverty line: $144,196.23 <br>
+        On $166k (median household income Auckland): $113,810.91 after tax (or $13k above poverty line) <br>
+    </p>
+    <h3>Family Tax Credits</h3>
+    <p>
+        See <a href="https://www.ird.govt.nz/working-for-families/types/family-tax-credit">the rules around family tax
+            credits</a> <br>
+        Eldest child: $7,524 <br>
+        Every other child: $6,130 <br>
+        Total: $13,654 <br>
+        “We work out the abatement by starting with your family income, subtracting $42,700, then multiplying the
+        difference by 27%. Your entitlement for the year will be your full family tax credit less the abatement.” <br>
+        <br>
+        $13,654/27% = $50,570.37 + $42,700 = $93,270.37 max income (no tax credit) <br>
+        This is below my calculated poverty line of $127k-$144k <br>
+
+        I don't think you'll get any other benefits based on that income. <br>
+        <br>
+        Interestingly, the max income where you get no benefits from Family Tax Credits lines up quite nicely with my
+        estimate of the New Zealand poverty line.
+    </p>
+    <h3>Final Thoughts</h3>
+    <p>
+        So on the whole, this simple anaylsis shows that it is indeed incredible expensive to live in New Zealand
+        relative to the wages you receieve. <br>
+        <br>
+        It shows that in Auckland you need a household income of 76% of the median in Auckland or almost 100% of the
+        national median household income to not be in poverty. That means across the country something like 50% of our
+        households are realstically in poverty.<br>
+        <br>
+        This also assumes you have only two kids and that there is no extra money for savings or holidays or birthdays
+        or any other random expenses. It is quite a bleak picture. Especially when you compare that to the good 'ol days
+        when just one income earner was enough to afford not only a povery lifestyle but a comfortable one which
+        incldued home ownership. <br>
+        <br>
+        Today to just reach the povery line on one income that is $144k gross income. Notable is that even though a big
+        percentage of our population seems to be below or near the povery line we don't have very high childcare or
+        healthcare costs which were really the two big killers in the US. In New Zealand that seems to be Housing and
+        Food (and our petrol is at least 2x as expensive as the US).
+    </p>
+</template>
+<style scoped>
+    header,
+    section {
+        max-width: 900px;
+        margin: auto;
+    }
+
+    img {
+        max-width: 100%;
+    }
+</style>
